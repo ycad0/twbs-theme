53929644058edba2dbfd703bcf72c7df6c6a17c8
Phillaf <phil.laf@gmail.com>
2015-06-30 20:57:27 -0400

    Default bootstrap theme for GintonicCMS. Build tools init

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    bowerRequirejs: {
+        options: {
+            transitive: true
+        },
+        target: {
+            rjsConfig: 'webroot/main.js'
+        }
+    },
+    requirejs: {
+      compile: {
+        options: {
+          appDir:"webroot",
+          baseUrl:"./",
+          dir:"webroot/js",
+          stubModules: ['jsx', 'text', 'JSXTransformer'],
+          paths: {
+              requireLib: '../node_modules/requirejs/require',
+          },
+          modules:[{
+            name: "main",
+            include: "requireLib"
+          }],
+          fileExclusionRegExp: /^.*\.(?!js$|jsx$)[^.]+$/,
+        }
+      }
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-bower-requirejs');
+
+  // Default task.
+  grunt.registerTask('default', ['bowerRequirejs','requirejs']);
+
+};
---
+++ webroot/main.js
@@ -0,0 +1,11 @@
+requirejs.config({
+    paths: {
+        bootstrap_theme: '/bootstrap/app/',
+    },
+    shim: {
+
+    },
+    packages: [
+
+    ]
+});
